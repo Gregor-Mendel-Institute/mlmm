1bcb1f92e730ffda1a80fcb9834a1addd878cbb8
Timothée Flutre <timflutre@gmail.com>
2016-05-23 17:27:45 +0200

    transform 'mlmm' into a proper R package (#5)

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Genotypes, SNP info, kinship and phenotypes.
+#'
+#' A dataset used as example for the mlmm function.
+#'
+#' @format A list with 4 components:
+#' \describe{
+#'   \item{X}{matrix of imputed genotypes}
+#'   \item{Y}{vector of phenotypes}
+#'   \item{K}{kinship matrix}
+#'   \item{snp_info}{SNP coordinates}
+#' }
+"example_data"
